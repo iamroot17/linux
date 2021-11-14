cad4417732e56ecc6e295240487a081da37a299b
JaeSang Yoo <jsyoo5b@gmail.com>
2021-11-14 18:55:39 +0900

    iamroot(qemu): exclude .gitkeep for initramfs
    * Exclude file which named .gitkeep (unnecessary for initramfs)
    * .gitkeep stands to keep directory structure to be tracked
    
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

--- qemu/create_initrd.sh
+++ qemu/create_initrd.sh
@@ -3,4 +3,4 @@ BUILDS="../images"
 mkdir -p $BUILDS;
 
 cd initramfs;
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > $BUILDS/initramfs.cpio.gz
+find . ! -name '.gitkeep' -print0 | cpio --null -ov --format=newc | gzip -9 > $BUILDS/initramfs.cpio.gz
